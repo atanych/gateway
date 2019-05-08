9d65f7cf2d1374c75d7fe893ca515297a7105ad2
atanych <atanych@gmail.com>
2019-05-09 00:59:41 +0300

    Add test for Inbox.Process

---
+++ test/gateway/inbox/process_test.exs
@@ -0,0 +1,37 @@
+defmodule Inbox.ProcessTest do
+  use GatewayWeb.ConnCase
+
+  test ".call" do
+    device = insert(:device, uniq_key: "rt4", transport: :telegram, settings: %{"token" => "asd"})
+
+    params = %{
+      transport: "telegram",
+      device_uniq_key: "rt4",
+      message: %{
+        chat: %{
+          first_name: "atanych",
+          id: 89_388_434,
+          type: "private",
+          username: "atanych"
+        },
+        date: 1_556_640_471,
+        from: %{
+          first_name: "atanych",
+          id: 89_388_434,
+          is_bot: false,
+          language_code: "en",
+          username: "atanych"
+        },
+        message_id: 2,
+        text: "hey"
+      },
+      update_id: 159_136_107
+    }
+
+    %{body: %{status: :ok}} = Inbox.Process.call(params)
+    event = InboxEvent |> Gateway.Repo.one()
+    assert event.device_id == device.id
+    assert event.data["client"]["id"] == 89_388_434
+    assert event.data["message"]["text"] == "hey"
+  end
+end
